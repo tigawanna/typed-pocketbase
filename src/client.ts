82caa03a2c44b9a5dd41a5c07ce978c9e2a21854
tigawanna <denniskinuthiaw@gmail.com>
2024-10-10 18:15:18 +0300

    add top level js doc

--- src/client.ts
+++ src/client.ts
@@ -9,6 +9,16 @@ import {
 import { TypedRecordService } from './client/record-service.js';
 
 export class TypedPocketBase<Schema extends GenericSchema> extends PocketBase {
+	/**
+	 * Creates a typed {@link TypedRecordService} instance for the given collection.
+	 *
+	 * @template CollectionName The name of the collection.
+	 * @template Collection The collection type. Defaults to `Schema[CollectionName]`.
+	 * @param name The name of the collection.
+	 * @returns A typed {@link ViewCollectionService} instance if the collection is a view,
+	 * a typed {@link BaseCollectionService} instance if the collection is a base collection,
+	 * or a typed {@link AuthCollectionService} instance if the collection is an auth collection.
+	 */
 	from<
 		CollectionName extends keyof Schema,
 		Collection extends GenericCollection = Schema[CollectionName]
@@ -21,9 +31,26 @@ export class TypedPocketBase<Schema extends GenericSchema> extends PocketBase {
 			: AuthCollectionService<Collection> {
 		return new TypedRecordService(this.collection(name as string)) as any;
 	}
+	/**
+	 * Returns a new instance of {@link TypedBatch} to be used for batching record operations.
+	 *
+	 * @returns A new instance of {@link TypedBatch}.
+	 */
 	fromBatch() {
 		return new TypedBatch<Schema>(this);
 	}
+	/**
+	 * Temporarily impersonates the provided record id as if it was the real authenticated user.
+	 *
+	 * Only works for `auth` collections.
+	 *
+	 * @param name - The name of the collection.
+	 * @param recordId - The id of the record to impersonate.
+	 * @param duration - The amount of milliseconds to impersonate for.
+	 * @param options - Additional options to pass to the underlying `impersonate` method.
+	 *
+	 * @returns A promise that resolves to the impersonated client instance if the collection is an `auth` collection, or `never` otherwise.
+	 */
 	impersonate<
 		CollectionName extends keyof Schema,
 		Collection extends GenericCollection = Schema[CollectionName]
